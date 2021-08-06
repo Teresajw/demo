1ef613e9c50ead2ea5e18c6fb092e5c128e3ba73
Teresajw <39899297+Teresajw@users.noreply.github.com>
2021-08-06 11:45:28 +0800

    update

--- src/main/java/com/teresaj/demo/controller/TeresajController.java
+++ src/main/java/com/teresaj/demo/controller/TeresajController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TeresajController {
     @GetMapping("/hello")
     public String test() {
-        return "Hello Docker 123!!!";
+        return "Hello Docker!! && Devops ~~~";
     }
 }
