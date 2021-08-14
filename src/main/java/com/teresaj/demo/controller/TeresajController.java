052c46ef80baf7bfff5991f596695b065bb0916e
Teresajw <39899297+Teresajw@users.noreply.github.com>
2021-08-14 16:08:25 +0800

    commit

--- src/main/java/com/teresaj/demo/controller/TeresajController.java
+++ src/main/java/com/teresaj/demo/controller/TeresajController.java
@@ -2,11 +2,8 @@ package com.teresaj.demo.controller;
 
 import com.teresaj.demo.entity.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,12 +16,14 @@ public class TeresajController {
     }
 
     @GetMapping("/getUserInfo")
-    public void test1() {
+    @ResponseBody
+    public List<User> test1() {
         List<User> list = new ArrayList<>();
+
         list.add(new User("张三", 23));
         list.add(new User("李四", 22));
         list.add(new User("王五", 25));
-        JSONArray userJson = JSONArray.parseArray(JSON.toJSONString(list));
-        userJson.forEach(User -> System.out.println(userJson.toJSONString()));
+
+        return list;
     }
 }
